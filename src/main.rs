21db81879b35217a139fff47563a19eb18f31f63
shubham-kshetre <shubhamkshetre@outlook.com>
2024-02-11 15:02:08 +0530

    feature: Adding support for images, audio and video

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate ipfs_api;
 
-use std::io::{self, Read};
+use std::io;
+use std::fs::File;
 use ipfs_api::IpfsClient;
 
 #[tokio::main]
@@ -14,13 +15,25 @@ async fn main() {
         Err(e) => eprintln!("Error getting IPFS version: {}", e),
     }
 
-    // Take user input for the data to be added to IPFS
-    let mut input = String::new();
-    println!("Enter the data to add to IPFS:");
-    io::stdin().read_to_string(&mut input).expect("Failed to read input");
+    // Take user input for the path of the file to be added to IPFS
+    let mut file_path = String::new();
+    println!("Enter the path of the file to add to IPFS:");
+    io::stdin().read_line(&mut file_path).expect("Failed to read input");
 
-    // Add the data to IPFS
-    match client.add(io::Cursor::new(input)).await {
+    // Trim the newline character from the input
+    let file_path = file_path.trim();
+
+    // Open the file
+    let file = match File::open(&file_path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Error opening file: {}", e);
+            return;
+        }
+    };
+
+    // Add the file to IPFS
+    match client.add(file).await {
         Ok(res) => {
             // Print the hash of the added content
             println!("Added to IPFS: {}", res.hash);
@@ -28,4 +41,3 @@ async fn main() {
         Err(e) => eprintln!("Error adding to IPFS: {}", e),
     }
 }
-
